9cb5d29251186940f778f06360c742ea24780410
laxman143 <laxmanprajapati143@gmail.com>
2026-01-28 10:04:31 +0530

    Added Login form

--- src/app/features/modules/auth/login/login.ts
+++ src/app/features/modules/auth/login/login.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  loginForm: FormGroup<{
+    email: FormControl<string>,
+    password: FormControl<string>;
+  }>
+
+  constructor(private fb:FormBuilder,private router:Router){
+  this.loginForm = this.fb.nonNullable.group({
+  email: ['', [Validators.required, Validators.email]],
+  password: ['', Validators.required]
+});
+  }
+
+  submit(){
+    localStorage.setItem("isLogin", "true");
+    this.router.navigate(['/'])
+  }
 }
